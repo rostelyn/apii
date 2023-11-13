7539de8aa02ffbefce7c38750738615d110717ca
Rostelyn Jane <rostelynjanea@gmail.com>
2023-11-13 17:00:46 +0800

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    public function users(){
+        $users = User ::all();
+        return response ($users);  
+      }
+
+      public function user($id){
+        $user = User::find($id);
+        return response ($user);  
+      }
+
+      public function delete($id){
+        $user = User::find($id);
+       if($user == null){
+        return response([
+            "message"=> "Record not found"
+        ],404);
+       }else{
+        $user->delete();
+        return response([
+            "message"=> "Delete Succesfully"
+        ],200);
+       }
+      }
+
+      
+}
+
+
